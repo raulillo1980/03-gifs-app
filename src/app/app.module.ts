6118fdeb3ef99f478cb96122324e0c9841503094
Raul Gomez Calcerrada <raul.gomez@swiftair.com>
2024-11-25 17:46:26 +0100

    ooo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
-import { GifsModule } from '../../docs/gifs/gifs.module';
+import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
