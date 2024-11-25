cdb9b3f349fabec39149f40851c0750e42470b3e
Raul Gomez Calcerrada <raul.gomez@swiftair.com>
2024-11-25 17:41:32 +0100

    sisi4

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { GifsService } from '../../../gifs/services/gifs.service';
+import { GifsService } from '../../../../../docs/gifs/services/gifs.service';
 
 @Component({
   selector: 'shared-sidebar',
